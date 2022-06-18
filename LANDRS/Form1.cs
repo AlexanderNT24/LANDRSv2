f23fff60b055246f3ae066fb845b6cfe6543f761
Alexander <89727568+AlexanderNT24@users.noreply.github.com>
2022-06-18 17:49:17 -0500

    Agregar archivos de proyecto.

---
+++ LANDRS/Form1.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+
+namespace LANDRS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            Registro obj=new Registro();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader obj;
+            string ruta = @"D:\Archivos\usuarios.txt";
+            obj = new StreamReader(ruta);
+            string contenido = obj.ReadToEnd();
+            obj.Close();
+
+            string[] arrayContenido = contenido.Trim().Split('\n');
+
+            for (int i = 0; i < arrayContenido.Length; i += 2)
+            {
+
+                if (arrayContenido[i].Contains(textBox1.Text))
+                {
+                   
+                    if (arrayContenido[i + 1].Contains(textBox2.Text))
+                    {
+                        Principal objVentana=new Principal();
+                        objVentana.Show();
+                        this.Hide();
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ LANDRS/Registro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LANDRS
+{
+    public partial class Registro : Form
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter fichero;
+            string ruta = @"D:\Archivos\usuarios.txt";
+            fichero = File.AppendText(ruta);
+            fichero.WriteLine(textBox1.Text);
+            fichero.WriteLine(textBox2.Text);
+            fichero.Close();
+
+            Form1 obj=new Form1();
+            obj.Show();
+            this.Hide();
+
+        }
+    }
+}
